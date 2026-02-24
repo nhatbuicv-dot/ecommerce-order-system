625e49cea55b355f2c5c76319c0f9ff76ace92e6
NhatBui <nhatbui22122005@gmail.com>
2026-02-24 18:04:04 +0700

    Initial commit

---
+++ ecommerce-order-system/src/main/java/com/ecommerce/platform/EcommerceOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceOrderSystemApplication.class, args);
+	}
+
+}
